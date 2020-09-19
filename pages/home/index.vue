b3de4412e810907bdd35e80c5b64bdec78f6dc01
YANGJIAKUN <200000725@163.com>
2020-09-19 15:35:47 +0800

    push

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,16 +1,39 @@
 <template>
     <view class="query-navbar">
-        这是home页面
+        <!-- 首页navbar左侧图标 -->
+        <view class="navbar-icon flex-center">
+            <starIcon name="bars" />
+        </view>
+        <!-- 首页navbar中间tab -->
+        <view class="navbar-tab flex-center">
+			
+		</view>
+        <!-- 首页navbar有侧图标 -->
+        <view class="navbar-icon flex-center">
+            <starIcon name="search" />
+        </view>
     </view>
 </template>
 <script>
+import starIcon from '@/components/star-icon'
 export default {
-    
+    components: {
+        starIcon,
+    },
 }
 </script>
 <style lang="less">
 .query-navbar {
     height: 90rpx;
     display: flex;
+    .navbar-icon {
+        width: 90rpx;
+        height: 90rpx;
+        justify-content: center;
+    }
+    .navbar-tab {
+        width: calc(100% - 180rpx);
+        height: 100%;
+    }
 }
 </style>
