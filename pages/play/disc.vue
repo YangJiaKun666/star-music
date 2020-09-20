68153f134a7cb166efeaec2f69ba2b976fc0cacf
gancao <gancao297871092@163.com>
2020-09-20 18:32:40 +0800

    疯狂打碟中

---
+++ pages/play/disc.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="disc">
+		<view class="disc disc-border" style="width: 525rpx;height: 525rpx;">
+			<view class="disc disc-border" style="width: 500rpx;height: 500rpx;">
+				<view class="disc disc-border" style="width: 475rpx;height: 475rpx;">
+					<view class="disc disc-border" style="width: 450rpx;height: 450rpx;">
+						<view class="disc disc-border" style="width: 425rpx;height: 425rpx;">
+							<view class="disc disc-border" style="width: 400rpx;height: 400rpx;">
+								<view class="disc disc-border" style="width: 375rpx;height: 375rpx;">
+									<view class="disc disc-border" style="width: 350rpx;height: 350rpx;">
+										<view class="disc disc-border" style="width: 325rpx;height: 325rpx;">
+											<image :src=imgUrl mode="scaleToFill" class="disc-image"/>
+										</view>
+									</view>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+    props: {
+        imgUrl: {
+            type: String,
+            default: '',
+        },
+       
+    }
+}
+</script>
+
+
+
+<style>
+	.disc{
+		width: 550rpx;
+		height: 550rpx;
+		background-color: #000;
+		border-radius: 275rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.disc-border{
+		border: 1rpx solid rgba(255,255,255,.2);
+	}
+	.disc-image{
+		width: 300rpx;
+		height: 300rpx;
+		border-radius: 150rpx;
+	}
+</style>
--- pages/play/index.vue
+++ pages/play/index.vue
@@ -1,33 +1,58 @@
 <template>
 	<view class="player-body">
+		<!-- 毛玻璃 -->
 		<helang-blur class="player-blur" :params="params"/>
-		<view class="player-head" :style="{ marginTop: statusBar + 'px' }">
+		<!-- 头部 -->
+		<view class="player-head" :style="{ marginTop: statusBar + 'rpx' }">
 		   <view class="navbar-icon flex-center">
 				<starIcon name="angle-left" size='50'/>
 			</view>
-			
 			<view class="songs">
 				<text class="songs-name">歌曲名字</text>
 				<text class="singer-name">歌手名字</text>
 			</view>
-	
 			<view class="navbar-icon flex-center">
 				<starIcon name="share-alt"  size='40'/>
 			</view>
 		</view>
-	
+		<!-- 轮播图--歌词 -->
+		<view @click="changePlayPanel" class="page-section-spacing" :style="{ marginTop: statusBar + 'rpx',height: windowHeight - 245 + 'px'}">
+			
+			<view style="width: 100%;height: 100%;" v-show="currentPlayPanel">
+				<view class="disc-mask-view">
+					<view class="dark-masker"/>
+				</view>
+				<swiper class="swiper">
+					<swiper-item>
+						<view class="swiper-item">
+							<disc :imgUrl=params.image />
+						</view>
+					</swiper-item>
+					<swiper-item>
+						<view class="swiper-item">
+							<disc :imgUrl=params.image />
+						</view>
+					</swiper-item>
+				</swiper>
+			</view>
+			
+			<view style="width: 100%;height: 100%;" v-show="!currentPlayPanel">歌词</view>
+			
+		</view>
 	
 	</view>
     
 </template>
 <script>
 import starIcon from '@/components/star-icon'
-import helangBlur from "@/components/helang-blur.vue"
+import helangBlur from "@/components/helang-blur"
+import disc from "./disc"
 
 export default {
     components: {
         starIcon,
-		helangBlur
+		helangBlur,
+		disc
     },
 	data() {
 	    return {
@@ -37,12 +62,19 @@ export default {
 				image:"https://mydarling.gitee.io/uniapp-extend/static/images/douyin/4.jpg",
 				blur:"xl"
 			},
-			statusBar: getApp().globalData.statusBar
+			currentPlayPanel: true,
+			statusBar: getApp().globalData.statusBar,
+			windowHeight: getApp().globalData.windowHeight
 		}
 	},
-	 onLoad: function () {
+	methods:{
+		changePlayPanel(){
+			this.$data.currentPlayPanel = !this.$data.currentPlayPanel
+		}
+	},
+	onLoad: function () {
 		// alert(getApp().globalData.windowHeight)
-	 }
+	}
 }
 </script>
 <style lang="less">
@@ -57,39 +89,74 @@ export default {
 		height: 100%;
 	}
 	.player-head {
-			width: 100%;
-		    height: 90rpx;
-			position: fixed;
-			z-index: 99;
-			top: 0;
-			left: 0;
-		    display: flex;
-			justify-content: space-between;
-			.songs{
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-				justify-content: center;
-				.songs-name{
-					color: #fff;
-					font-size: 32rpx;
-				}
-				.singer-name{
-					color: #fff;
-					font-size: 28rpx;
-					opacity: .8;
-				}
-			}
-		    .navbar-icon {
-		        width: 90rpx;
-		        height: 90rpx;
+		width: 100%;
+		height: 90rpx;
+		position: fixed;
+		z-index: 99;
+		top: 0;
+		left: 0;
+		display: flex;
+		justify-content: space-between;
+		.songs{
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			.songs-name{
 				color: #fff;
-		        justify-content: center;
-		    }
-		    .navbar-tab {
-		        width: calc(100% - 180rpx);
-		        height: 100%;
-		    }
+				font-size: 32rpx;
+			}
+			.singer-name{
+				color: #fff;
+				font-size: 28rpx;
+				opacity: .8;
+			}
+		}
+		.navbar-icon {
+			width: 90rpx;
+			height: 90rpx;
+			color: #fff;
+			justify-content: center;
+		}
+		.navbar-tab {
+			width: calc(100% - 180rpx);
+			height: 100%;
+		}
+	}
+	.page-section-spacing{
+		position: fixed;
+		z-index: 1;
+		top: 90rpx;
+		width: 100%;
+	}
+	.swiper{
+		height: 100%;
+	}
+	.swiper-item{
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.disc-mask-view{
+		width: 600rpx;
+		height: 600rpx;
+		background-color: rgba(255,255,255,.2);
+		border-radius: 300rpx;
+		border: 1rpx solid rgba(255,255,255,.4);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		left: calc(50% - 300rpx);
+		top: calc(50% - 300rpx);
+	}
+	.dark-masker{
+		width: 550rpx;
+		height: 550rpx;
+		background-color: rgba(0,0,0,.25);
+		border-radius: 275rpx;
 	}
 }
 
